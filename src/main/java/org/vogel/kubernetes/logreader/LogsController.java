27262f9cb49ce3e4cf8b7b8df328a8188e2eb06b
Chris Vogel <cjvogel1972@gmail.com>
2018-05-16 17:33:56 -0500

    Use a pre tag and don't parse logs
    Make it a little easier.

--- src/main/java/org/vogel/kubernetes/logreader/LogsController.java
+++ src/main/java/org/vogel/kubernetes/logreader/LogsController.java
@@ -37,9 +37,7 @@ public class LogsController {
     public String showPodLogs(@RequestParam @NotNull String podName, Model model) {
         LOGGER.debug("In showPodLogs with namespace: {} and pod: {}", namespace, podName);
         try {
-            String logs = KubernetesUtils.getLogs(namespace, podName)
-                    .replace("\n", "<br>")
-                    .replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+            String logs = KubernetesUtils.getLogs(namespace, podName);
             model.addAttribute("logs", logs);
             model.addAttribute("podName", podName);
             return "logs";
