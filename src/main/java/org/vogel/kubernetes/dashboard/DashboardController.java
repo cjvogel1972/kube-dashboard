2688d094c0f269650b0fbe4df5216b5285803575
Chris Vogel <cjvogel1972@gmail.com>
2018-05-31 10:56:52 -0500

    Use lombok slf4j
    Simplify the code.

--- src/main/java/org/vogel/kubernetes/dashboard/DashboardController.java
+++ src/main/java/org/vogel/kubernetes/dashboard/DashboardController.java
@@ -1,23 +1,21 @@
 package org.vogel.kubernetes.dashboard;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class DashboardController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DashboardController.class);
 
     @Value("${my.pod.namespace:default}")
     private String namespace;
 
     @GetMapping("/")
     public String index(Model model) {
-        LOGGER.debug("In index with namespace: {}", namespace);
+        log.debug("In index with namespace: {}", namespace);
         model.addAttribute("namespace", namespace);
         return "index";
     }
--- src/main/java/org/vogel/kubernetes/dashboard/PodController.java
+++ src/main/java/org/vogel/kubernetes/dashboard/PodController.java
@@ -1,8 +1,7 @@
 package org.vogel.kubernetes.dashboard;
 
 import io.kubernetes.client.ApiException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,50 +11,49 @@ import org.springframework.web.bind.annotation.PathVariable;
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
 
+@Slf4j
 @Controller
 public class PodController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(PodController.class);
 
     @Value("${my.pod.namespace:default}")
     private String namespace;
 
     @GetMapping("/pods")
     public String listPods(Model model) {
-        LOGGER.debug("In listPods with namespace: {}", namespace);
+        log.debug("In listPods with namespace: {}", namespace);
         try {
             model.addAttribute("pods", KubernetesUtils.getPods(namespace));
             model.addAttribute("namespace", namespace);
             return "pods";
         } catch (IOException | ApiException e) {
-            LOGGER.error("Error getting list of pods", e);
+            log.error("Error getting list of pods", e);
             return "error";
         }
     }
 
     @GetMapping("/pods/{podName}")
     public String describePod(@PathVariable @NotNull String podName, Model model) {
-        LOGGER.debug("In describePod with namespace: {} and pod: {}", namespace, podName);
+        log.debug("In describePod with namespace: {} and pod: {}", namespace, podName);
         try {
             model.addAttribute("pod", KubernetesUtils.getPod(namespace, podName));
             model.addAttribute("podName", podName);
             return "pod_describe";
         } catch (IOException | ApiException e) {
-            LOGGER.error("Error getting pod", e);
+            log.error("Error getting pod", e);
             return "error";
         }
     }
 
     @GetMapping("/pods/{podName}/logs")
     public String showPodLogs(@PathVariable @NotNull String podName, Model model) {
-        LOGGER.debug("In showPodLogs with namespace: {} and pod: {}", namespace, podName);
+        log.debug("In showPodLogs with namespace: {} and pod: {}", namespace, podName);
         try {
             String logs = KubernetesUtils.getPodLogs(namespace, podName);
             model.addAttribute("logs", logs);
             model.addAttribute("podName", podName);
             return "logs";
         } catch (IOException | ApiException e) {
-            LOGGER.error("Error getting logs for pod {}", podName, e);
+            log.error("Error getting logs for pod {}", podName, e);
             return "error";
         }
     }
