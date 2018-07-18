7d17b4b09d7a97141f122fcc63da8b368a7c11da
Chris Vogel <cjvogel1972@gmail.com>
2018-07-18 13:16:07 -0600

    Rename the application class
    The last thing (I think) that we need to change to finalize the rename
    of the project.

--- src/main/java/org/vogel/kubernetes/dashboard/KubeDashboardApplication.java
+++ src/main/java/org/vogel/kubernetes/dashboard/KubeDashboardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogReaderApplication {
+public class KubeDashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LogReaderApplication.class, args);
+        SpringApplication.run(KubeDashboardApplication.class, args);
     }
 
 }
--- src/test/java/org/vogel/kubernetes/dashboard/KubeDashboardApplicationTests.java
+++ src/test/java/org/vogel/kubernetes/dashboard/KubeDashboardApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LogReaderApplicationTests {
+public class KubeDashboardApplicationTests {
 
     @Test
     public void contextLoads() {
